9c89901edf8889601e6e90404b5c506381be5d95
roxerj <juozapas.c@gmail.com>
2024-04-07 23:02:08 +0300

    moved 'quotes' and 'tips' from python script to a json file

--- greenliving-code/weekly_green_quotes.py
+++ greenliving-code/weekly_green_quotes.py
@@ -1,23 +1,22 @@
 import datetime
+import json
+import os
+
+
+# Get the current directory of the Py script
+current_directory = os.path.dirname(os.path.realpath(__file__))
+json_file_path = os.path.join(current_directory, 'quotes_and_tips.json')
 
 # Get the current day
 current_day = datetime.datetime.now().strftime("%A")
-quotes = {"Monday": "Being green and clean is not just an aspiration but an action. -Christine Pelosi", 
-"Tuesday": "The future will be either green or not at all. -Bob Brown",
-"Wednesday": "Going green doesn't start with doing green acts, it starts with a shift in consciousness. -Ian Somerhalder", 
-"Thursday": "Green technologies - going green - is bigger than the Internet. It could be the biggest economic opportunity of the 21st century. -Narendra Modi", 
-"Friday": "I think any opportunity you have to be green, whether it's in business or in everyday life, you should take it. -Lauren Conrad", 
-"Saturday": "Lack of time and money create really bad green practices. -Ian Somerhalder", 
-"Sunday": "Green calm below, blue quietness above. - John Greenleaf Whittier"}
+
+# Read the JSON file
+with open(json_file_path, 'r') as file:
+    data = json.load(file)
+
+# Access the quotes and tips based on the current day
+quotes = data["quotes"]
+tips = data["tips"]
 
 print ("Quote for", current_day + ":", quotes[current_day])
-
-tips = {"Monday": "Be careful of the water you spill. Water is life",
-        "Tuesday": "Did you know that there are thrift shops and second-hand stores that have pricey brands at a very affordable price? Check them out",
-        "Wednesday": "Don't forget to turn of the lights on your way out today!",
-        "Thursday": "Thursday is the best day to make a lunch from all the scraps you have in your fridge. Don't let them go bad!",
-        "Friday": "It's friday, yeah! Make sure to recycle before that rave party you have going on!",
-        "Saturday": "You woke up late from yesterday's rave, but you sure won't forget of that beach cleaning you have planned at 4, will you?",
-        "Sunday": "Dude, your plants are screaming! Water them!"}
-
 print ("Daily Green tip:", tips[current_day])
